b2405ad841794c910838b4a526bbc2fca0600ea2
Andreas Kaiser <disko@binary-punks.com>
2015-07-28 11:46:06 +0200

    Fix broken tests.

--- kotti_content_proxy/tests/test_models.py
+++ kotti_content_proxy/tests/test_models.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 
 
-def test_model(root, db_session):
+def test_model(root, db_session, content):
 
     from kotti.resources import Document
     from kotti_content_proxy.resources import ContentProxy
 
-    doc = Document.query.filter(Document.id == 2).one()
+    root[u'foo'] = Document(title=u'Foo')
+    db_session.flush()
+
+    doc = root[u'foo']
 
     # create a proxy for the "about" document
-    root['proxy'] = ContentProxy(proxied_id=2, title="proxied about")
+    root['proxy'] = ContentProxy(proxied_id=doc.id, title="proxied about")
     db_session.flush()
 
     assert root['proxy'].proxied_object == doc
