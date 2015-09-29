95e13bb888fc3608bb5fd0cf9431985234326dd7
Piotr Dobrogost <pd@dev.dobrogost.net>
2015-09-29 15:00:31 +0200

    Converted to use 'foo_proxy' fixture.

--- kotti_content_proxy/tests/test_models.py
+++ kotti_content_proxy/tests/test_models.py
@@ -1,18 +1,5 @@
 # -*- coding: utf-8 -*-
 
 
-def test_model(root, db_session, content):
-
-    from kotti.resources import Document
-    from kotti_content_proxy.resources import ContentProxy
-
-    root[u'foo'] = Document(title=u'Foo')
-    db_session.flush()
-
-    doc = root[u'foo']
-
-    # create a proxy for the document
-    root['proxy'] = ContentProxy(proxied_id=doc.id, title=u'proxied about')
-    db_session.flush()
-
-    assert root['proxy'].proxied_object == doc
+def test_model(root, foo_proxy):
+    assert root['proxy'].proxied_object == root[u'foo']
